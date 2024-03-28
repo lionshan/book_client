44508bc695b51922672d9e7e5279cc3e53233fc0
shiyangWork <yang.danshi@ly.com>
2024-03-28 20:23:37 +0800

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home_view">
+    <h1>Home Page</h1>
+    <p>Home page content</p>
+  </div>
+</template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue';
+onMounted(() => {
+  //console.log('3.-组件挂载到页面之后执行-------onMounted')
+})
+</script>
+<style scoped lang='scss'>
+</style>
